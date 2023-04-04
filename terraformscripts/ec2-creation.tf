9e76bd6d14ee68e31d662da5c102de5d08c99262
saitharun6 <125140575+saitharun6@users.noreply.github.com>
2023-04-05 00:39:14 +0400

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,15 +1,17 @@
-provider "aws"{
-region = "ap-southeast-1"
+provider "aws"
+{
+region="us-east-1"
+}
+resource"aws_instance" "AWSInstance"
+{
+  ami="ami-016eb5d644c333ccb"
+  instance-type="t2.micro"
+  key_name="mydevopstraining"
+  security_groups=["launch-wizard-2","launch-wizard-7","launch-wizard-5"]
+  tags={
+   Name="tomcatservers"
+  }
 }
 
-resource "aws_instance" "AWSInstance" {
-     ami = "ami-0319ac76374b9fe74"
-	 instance_type = "t2.micro"
-	 key_name = "devopsmss"
-	 security_groups = ["launch-wizard-2"]
-	 tags = {
-	 Name = "tomcatservers"
-	 }
-}
 
 
